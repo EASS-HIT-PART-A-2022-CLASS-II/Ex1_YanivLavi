6108f5ef699b6746fc2d40e62c00c9d6f3703500
yanivlav <yan465@gamil.com>
2022-12-26 18:53:05 +0200

    updated backend ip address in frontend

--- frontend/main.py
+++ frontend/main.py
@@ -7,11 +7,11 @@ data_type = st.radio("Select data type:", ("Track", "Playlist"))
 if st.button("Get data"):
     if data_type == "Track":
         data = requests.get(
-            f"http://localhost:8989/tracks/{id_input}").json()
+            f"http://172.20.0.2:8080/tracks/{id_input}").json()
         st.write("Track data:", data)
     elif data_type == "Playlist":
         data = requests.get(
-            f"http://localhost:8989/playlists/{id_input}").json()
+            f"http://172.20.0.2:8080/playlists/{id_input}").json()
         st.write("Playlist data:", data)
 
 # def main():
